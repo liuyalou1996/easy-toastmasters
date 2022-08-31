394ecb3f8310450177af36ff13dfeb570d772024
Nick Liu <2462405721@qq.com>
2022-08-31 18:37:07 +0800

    feat: 加入Echarts图形Demo

--- src/main/resources/templates/ah-counter/js/request.js
+++ src/main/resources/templates/ah-counter/js/request.js
@@ -2,16 +2,15 @@
  * 发送get请求
  * @param url
  * @param data
- * @param callbackOnSuccess
- * @param callbackOnFailure
+ * @param successCallback
+ * @param failureCallback
  */
-function get(url, data, callbackOnSuccess, callbackOnFailure) {
+function get(url, data, successCallback, failureCallback) {
     axios.get(url, {params: data}).then(response => {
         if (response.status !== 200) {
             layer.error("Fail to send request!");
         }
-        callbackOnSuccess(response.data);
-    }).then(callbackOnFailure);
+    }).catch(error => layer.msg("Request failed!"));
 }
 
 /**
@@ -19,13 +18,28 @@ function get(url, data, callbackOnSuccess, callbackOnFailure) {
  * @param url
  * @param data
  * @param callbackOnSuccess
- * @param callbackOnFailure
  */
-function post(url, data, callbackOnSuccess, callbackOnFailure) {
+function post(url, data, callbackOnSuccess) {
     axios.post(url, data, {headers: {'content-type': 'application/json'}}).then(response => {
         if (response.status !== 200) {
             layer.error("Fail to send request!");
         }
         callbackOnSuccess(response.data);
-    }).then(callbackOnFailure);
+    }).catch(error => layer.msg("Request failed!"));
+}
+
+
+function getAhWordsMapping(reportNo) {
+    let url = `/ah-counter/report/ah-words-mapping/${reportNo}`;
+    return axios.get(url, {params: null})
+}
+
+function getAhCounterReportOverview(reportNo, callback) {
+    let url = `/ah-counter/report/overview/${reportNo}`;
+    return axios.get(url, {params: null})
+}
+
+function getAhCounterReportDetail(reportNo, callback) {
+    let url = `/ah-counter/report/detail/${reportNo}`;
+    return axios.get(url, {params: null})
 }
