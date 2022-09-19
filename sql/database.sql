81b5f3ada4206dc3a7c22176a7cc2f21e54edbdf
Nick Liu <2462405721@qq.com>
2022-09-19 14:09:16 +0800

    fix: 修改数据库脚本

--- sql/database.sql
+++ sql/database.sql
@@ -9,9 +9,9 @@ create table t_ah_counter_report
     total_used            smallint      default 0                 not null comment '哼哈词总使用次数',
     used_info_per_guest   mediumtext                              not null comment '每位嘉宾哼哈词使用情况',
     used_word_and_count   varchar(1024) default ''                not null comment '哼哈词及使用次数映射',
-    most_used_word_top3   varchar(256)  default ''                not null comment '哼哈词使用频率最高前3，以JSON格式保存',
+    most_used_word_top5   varchar(256)  default ''                not null comment '哼哈词使用频率最高前5，以JSON格式保存',
     used_guest_and_count  varchar(256)  default ''                not null comment '哼哈词使用嘉宾和次数，以JSON格式保存',
-    most_used_guest_top3  varchar(256)  default ''                not null comment '哼哈词使用次数最多嘉宾前3，以JSON格式保存',
+    most_used_guest_top5  varchar(256)  default ''                not null comment '哼哈词使用次数最多嘉宾前5，以JSON格式保存',
     least_used_guest_top3 varchar(256)  default ''                not null comment '哼哈词使用次数最少嘉宾前3，以JSON格式保存',
     create_time           datetime      default CURRENT_TIMESTAMP not null comment '创建时间'
 ) comment 'Ah-counter统计报告表';
